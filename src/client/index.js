9708990a3533694dd4feb2c37658082ab3df44b7
mindcitycode <mindcity@proton.me>
2022-10-22 22:09:38 +0200

    websockets + cors

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,46 @@
-console.log('ok')
+const makeRestUrl = (pathname) => {
+    const url = new URL(window.location)
+    //url.protocol = 'ws'
+    url.port = '3000'
+    url.pathname = pathname
+    return url.toString()
+}
+
+async function rest() {
+    const url = makeRestUrl('zozo')
+    const a = await fetch(url)
+    console.log('fetched', await a.json())
+}
+rest()
+
+const makeWsUrl = () => {
+    const url = new URL(window.location)
+    url.protocol = 'ws'
+    url.port = '3000'
+    url.pathname = '/hello-ws'
+    return url.toString()
+}
+
+function websocket() {
+    console.log(makeWsUrl())
+
+    const socket = new WebSocket(makeWsUrl())
+    console.log(socket)
+
+    socket.addEventListener('open', function (event) {
+        console.log('socket is opened')
+        socket.send('Coucou le serveur !');
+    });
+    socket.addEventListener('close', function (event) {
+        console.log('Byebye le serveur !');
+    });
+    socket.addEventListener('message', function (event) {
+        console.log('Voici un message du serveur', event.data);
+    });
+
+    socket.addEventListener('error', function (event) {
+        console.log('Voici un message de erreur', event);
+    });
+
+}
+websocket()
--- src/server/index.js
+++ src/server/index.js
@@ -1,21 +1,39 @@
 import { networkInterfaces } from 'os'
 import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import fastifyWebsocket from '@fastify/websocket'
 
 const [localNetworkIpV4] = Object.values(networkInterfaces()).flat().filter(({ netmask }) => netmask === '255.255.255.0').map(({ address }) => address)
 const port = process.env.PORT || 3000
 const host = process.env.HOST || localNetworkIpV4
 
-const fastify = Fastify({
-    logger: true
-})
+const go = async () => {
 
-fastify.get('/', async (request, reply) => {
-    reply.type('application/json').code(200)
-    return { hello: 'world' }
-})
+    const fastify = Fastify({
+        logger: true
+    })
 
-fastify.listen({ port, host }, (err, address) => {
-    if (err) throw err
-    // Server is now listening on ${address}
-})
+    await fastify.register(cors, {
+        // put your options here
+    })
+  await  fastify.register(fastifyWebsocket);
 
+    fastify.get('/hello-ws', { websocket: true }, (connection, req) => {
+        connection.socket.send("hello from server")
+        connection.socket.on('message', message => {
+            console.log(message.toString())
+            connection.socket.send('Hello Fastify WebSockets');
+        });
+    });
+
+    fastify.get('/zozo', async (request, reply) => {
+        reply.type('application/json').code(200)
+        return { hello: 'world' }
+    })
+
+    fastify.listen({ port, host }, (err, address) => {
+        if (err) throw err
+        // Server is now listening on ${address}
+    })
+}
+go()
