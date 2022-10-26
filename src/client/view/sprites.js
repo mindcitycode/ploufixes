8caad1a0c61129f5824c81228d8673e14421edc5
mindcitycode <mindcity@proton.me>
2022-10-26 17:52:19 +0200

    load combined assets

--- src/client/view/sprites.js
+++ src/client/view/sprites.js
@@ -1,14 +1,56 @@
 import * as PIXI from 'pixi.js'
 const packBasePath = 'assets/Robot Warfare Asset Pack 22-11-24'
 const packSpriteSheetsFilename = 'spritesheets.json'
+const combinedJsonSpriteSheetFilename = 'combined.json'
+
+export const loadSpritesheet = async (atlasName) => {
+    const altasPath = [packBasePath, atlasName].join('/')
+    const atlasData = await fetch(altasPath).then(x => x.json())
+    const imagePath = [packBasePath, atlasData.meta.image].join('/')
+    const spritesheet = new PIXI.Spritesheet(
+        PIXI.BaseTexture.from(imagePath),
+        atlasData
+    );
+    await spritesheet.parse()
+    return { atlasData, spritesheet }
+}
+
 export const loadSprites = async (app) => {
+
+    const { atlasData, spritesheet } = await loadSpritesheet('combined.json')
+    let x = 0
+    let y = 0
+
+    Object.keys(atlasData.animations).forEach(animationName => {
+        console.log(animationName)
+        const asprite = new PIXI.AnimatedSprite(spritesheet.animations[animationName]);
+
+        asprite.animationSpeed = 0.1
+        asprite.x = x
+        asprite.y = y
+        x += 32
+        if (x > (7 * 32)) {
+            x = 0
+            y += 32
+        }
+
+        app.stage.addChild(asprite)
+        asprite.gotoAndPlay(0)
+
+    })
+
+}
+
+export const loadSprites2 = async (app) => {
 
     const categorisedSpritesheets = await fetch(`${packBasePath}/${packSpriteSheetsFilename}`).then(x => x.json())
 
     const parses = []
-    const animations = {}
+    const allAnimations = {}
     let x = 0
     let y = 0
+
+
     Object.entries(categorisedSpritesheets).forEach(async ([category, spritesheets], i) => {
         Object.entries(spritesheets).forEach(async ([spritesheetName, atlasData], j) => {
             if (Object.values(atlasData.animations).length) {
@@ -19,14 +61,21 @@ export const loadSprites = async (app) => {
                 );
 
                 await spritesheet.parse()
+
                 Object.keys(atlasData.animations).forEach(animationName => {
+
+                    const animationFullName = `${category}.${spritesheetName}.${animationName}`
+
+                    console.log('AAA', spritesheet.animations[animationName])
+
+
                     const firstAnimName = [0];
                     const asprite = new PIXI.AnimatedSprite(spritesheet.animations[animationName]);
                     asprite.animationSpeed = 0.1
                     asprite.x = x
                     asprite.y = y
                     x += 32
-                    if (x > 200) {
+                    if (x > (7 * 32)) {
                         x = 0
                         y += 32
                     }
