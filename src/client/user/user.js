4e03aae2f2835d28ff812860b501ac3b9da3ce69
mindcitycode <mindcity@proton.me>
2022-11-02 23:02:10 +0100

    use router for login/sign-in/play

--- src/client/user/user.js
+++ src/client/user/user.js
@@ -1,12 +1,11 @@
 import './user.css'
 export const getCurrentUser = async () => fetch('current-user').then(x => x.json())
-export const loginForm = ({ message }) => {
-    return new Promise((accept, reject) => {
-        document.body.innerHTML = `
+export const loginForm = ({ message = '' } = {}) => {
+    document.body.innerHTML = `
     <div id="login">
     <form action="/login" method="post">
     <h3>Login</h3>
-    <p class="bad-login-message">${message}</p>
+    <p id="bad-login-message">${message}</p>
         <label for="username">Username</label>
         <input placeholder="username" id="username" name="username" type="text" autocomplete="username" required autofocus>
         <label for="current-password">Password</label>
@@ -16,52 +15,52 @@ export const loginForm = ({ message }) => {
     </form>    
     </div>
     `
-        const $submitButton = document.getElementById('submit-login')
-        const $usernameInput = document.getElementById('username')
-        const $usernamePassword = document.getElementById('current-password')
-        const postLogin = async (username, password) => {
-            const reply = await fetch("/login", {
-                method: "post",
-                body: new URLSearchParams({ username, password }),
-                headers: { "Content-Type": "application/x-www-form-urlencoded" }
-            })
-            document.body.innerHTML = ''
-            console.log(reply)
-            if (reply.status === 200) {
-                accept({ reply })
-            } else {
-                reject({ reply, message: 'wrong username or password' })
-            }
+    const $badLoginMessageParagraph = document.getElementById('bad-login-message')
+    const $submitButton = document.getElementById('submit-login')
+    const $usernameInput = document.getElementById('username')
+    const $usernamePassword = document.getElementById('current-password')
+    const postLogin = async (username, password) => {
+        const reply = await fetch("/login", {
+            method: "post",
+            body: new URLSearchParams({ username, password }),
+            headers: { "Content-Type": "application/x-www-form-urlencoded" }
+        })
+        console.log(reply)
+        if (reply.status === 200) {
+            window.router.goto('#play')
+        } else {
+            $badLoginMessageParagraph.textContent = 'wrong username or password'
         }
-        $submitButton.onclick = (e) => {
-            console.log([$usernameInput.value, $usernamePassword.value])
-            postLogin($usernameInput.value, $usernamePassword.value)
-            e.preventDefault()
-        }
+    }
+    $submitButton.onclick = (e) => {
+        e.preventDefault()
+        console.log([$usernameInput.value, $usernamePassword.value])
+        postLogin($usernameInput.value, $usernamePassword.value)
+    }
 
-        const $signUpLink = document.getElementById('link-sign-up')
-        console.log({$signUpLink})
-        $signUpLink.onclick = async e => {
-            e.preventDefault()
-            console.log('sign up')
-            document.body.innerHTML = ''
-            await SignUpForm({})
-        }
+    const $signUpLink = document.getElementById('link-sign-up')
+    console.log({ $signUpLink })
+    $signUpLink.onclick = async e => {
+        e.preventDefault()
+        window.router.goto('#signup')
+    }
 
-    })
+    return () => {
+        document.body.innerHTML = 'cleanup'
+    }
 
 }
 
 //    <input type="hidden" name="_csrf" value="<%= csrfToken %>">
 
 
-export const SignUpForm = ({ message }) => {
-    return new Promise((accept, reject) => {
-        document.body.innerHTML = `
+export const SignUpForm = ({ message = '' } = {}) => {
+
+    document.body.innerHTML = `
     <div id="login">
     <form action="/signup" method="post">
     <h3>Sign up</h3>
-    <p class="bad-login-message">${message}</p>
+    <p id="bad-signup-message">${message}</p>
         <label for="username">Username</label>
         <input placeholder="username" id="username" name="username" type="text" autocomplete="username" required autofocus>
         <label for="current-password">Password</label>
@@ -71,36 +70,46 @@ export const SignUpForm = ({ message }) => {
     </form>    
     </div>
     `
-        const $submitButton = document.getElementById('submit-signup')
-        const $usernameInput = document.getElementById('username')
-        const $usernamePassword = document.getElementById('current-password')
-        const postLogin = async (username, password) => {
-            const reply = await fetch("/signup", {
-                method: "post",
-                body: new URLSearchParams({ username, password }),
-                headers: { "Content-Type": "application/x-www-form-urlencoded" }
-            })
-            document.body.innerHTML = ''
-            console.log(reply)
-            if (reply.status === 200) {
-                accept({ reply })
+    const $badSignupMessageParagraph = document.getElementById('bad-signup-message')
+    const $submitButton = document.getElementById('submit-signup')
+    const $usernameInput = document.getElementById('username')
+    const $usernamePassword = document.getElementById('current-password')
+    const postSignup = async (username, password) => {
+        $badSignupMessageParagraph.classList.remove('shake')
+
+        const reply = await fetch("/signup", {
+            method: "post",
+            body: new URLSearchParams({ username, password }),
+            headers: { "Content-Type": "application/x-www-form-urlencoded" }
+        })
+        console.log(reply)
+        if (reply.status === 200) {
+            const r = await reply.json()
+            if (r.error) {
+                $badSignupMessageParagraph.textContent = r.error
+                $badSignupMessageParagraph.classList.add('shake')
             } else {
-                reject({ reply, message: 'wrong username or password' })
+                router.goto('#login')
             }
+        } else {
+            $badSignupMessageParagraph.textContent = 'error!'
+            $badSignupMessageParagraph.classList.add('shake')
         }
-        $submitButton.onclick = (e) => {
-            console.log([$usernameInput.value, $usernamePassword.value])
-            postLogin($usernameInput.value, $usernamePassword.value)
-            e.preventDefault()
-        }
+    }
+    $submitButton.onclick = (e) => {
+        console.log([$usernameInput.value, $usernamePassword.value])
+        postSignup($usernameInput.value, $usernamePassword.value)
+        e.preventDefault()
+    }
 
-        const $LoginLink = document.getElementById('link-login')
-        console.log({$LoginLink})
-        $LoginLink.onclick = e => {
-            console.log('login')
-            e.preventDefault()
-        }
+    const $LoginLink = document.getElementById('link-login')
+    console.log({ $LoginLink })
+    $LoginLink.onclick = e => {
+        e.preventDefault()
+        window.router.goto('#login')
+    }
 
-    })
-
+    return () => {
+        document.body.innerHTML = ''
+    }
 }
