c4db0799c1d9a59168cc0b1f1b968376429ac1f1
mindcitycode <mindcity@proton.me>
2022-11-03 10:44:34 +0100

    hash passwords wth bcrypt

--- src/server/users.js
+++ src/server/users.js
@@ -2,14 +2,18 @@ import { v4 as uuidv4 } from 'uuid'
 
 const Users = new Map()
 
+import bcrypt from 'bcrypt'
+const salt = bcrypt.genSaltSync(10); // store in db
+
 export const addOne = async (username, password) => {
     console.log('addOne', username, password)
     const existing = await findByUsername(username)
     if (existing === undefined) {
         const id = uuidv4()
-        const user = { id, username, password, creationDate : Date.now() }
+        const passwordHash = await bcrypt.hash(password, salt)
+        const user = { id, username, passwordHash, creationDate: Date.now() }
         Users.set(id, user)
-        console.log('add users after creation',Users)
+        console.log('add users after creation', Users)
         return user
     }
 }
@@ -24,7 +28,8 @@ export const findByUsername = async username => {
     }
 }
 export const verifyPassword = async (user, password) => {
-    return user.password === password
+    const res = await bcrypt.compare(password, user.passwordHash)
+    return res // true or false
 }
 
 
